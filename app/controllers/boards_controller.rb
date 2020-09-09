35b2ae4b94598ced8fbff51dd1aec096d6915685
tidus1107 <66785728+tidus1107@users.noreply.github.com>
2020-09-09 23:14:16 +0900

    board一覧を表示できるようにした

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,14 +3,30 @@ class BoardsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
-        render 'boards/index'
+      @boards = Board.all
+    end
+
+    def show
+      @board = Board.find(params[:id])
     end
 
     def new
-        @board = Board.new
+      @board = Board.new
     end
 
     def create
-        
+      @board = Board.new(board_params)
+      @board.user = current_user
+      if @board.save!
+          redirect_to board_path(@board), notice: '保存しました'
+      else
+          flash.now[:error] = '保存に失敗しました'
+          render 'boards/new'
+      end
+    end
+
+    private
+    def board_params
+      params.require(:board).permit(:name, :description)
     end
 end
