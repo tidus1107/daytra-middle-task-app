5f327e4c8e2ddfffc2bad03baa60a9a77e5514d1
tidus1107 <66785728+tidus1107@users.noreply.github.com>
2020-10-01 23:56:35 +0900

    new・createアクションを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,13 +4,30 @@ class TasksController < ApplicationController
     # def index
     # end
 
-    # def show
-    #   board = Board.find(params[:board_id])
-    #   @tasks = current_user.board
-    # end
+    def show
+      board = Board.find(params[:board_id])
+      @tasks = board.task.find(params[:id])
+    end
 
     def new
       board = Board.find(params[:board_id])
-      @task = current_user.board.tasks.build
+      @task = board.tasks.build
+    end
+
+    def create
+      board = Board.find(params[:board_id])
+      @task = board.tasks.build(task_params)
+      @task.user = current_user
+      if @task.save
+          redirect_to board_tasks_path(@task), notice: '保存しました'
+      else
+          flash.now[:error] = '保存に失敗しました'
+          render :new
+      end
+    end
+
+    private
+    def task_params
+          params.require(:task).permit(:title, :content)
     end
 end
