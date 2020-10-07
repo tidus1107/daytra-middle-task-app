a2cbedf9a3eb1efab5352e504a0d7b03540a8356
tidus1107 <66785728+tidus1107@users.noreply.github.com>
2020-10-07 22:25:54 +0900

    createアクションを追加 task#showに表示できるように変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,22 @@ class CommentsController < ApplicationController
         task = board.tasks.find(params[:task_id])
         @comment = task.comments.build
     end
+
+    def create
+        board = Board.find(params[:board_id])
+        task = board.tasks.find(params[:task_id])
+        @comment = task.comments.build(comment_params)
+        @comment.user = current_user
+        if @comment.save!
+            redirect_to board_task_path(id: task.id), notice: 'コメントしました'
+        else
+            flash.now[:error] = 'コメント出来ませんでした'
+            render :new
+        end
+    end
+
+    private
+    def comment_params
+          params.require(:comment).permit(:content)
+    end
 end
